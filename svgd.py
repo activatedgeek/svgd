a683ba172a5b96609354ea50dfccf92b6ace3206
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2019-03-30 20:53:26 -0400

    Ability to use any optimizer for svgd

--- svgd.py
+++ svgd.py
@@ -1,38 +1,31 @@
 import torch
+import torch.autograd as autograd
+
 
 class SVGD:
-  def __init__(self, P, K, eta=1e-2, rho=0.9):
+  def __init__(self, P, K, optimizer):
     self.P = P
     self.K = K
-    self.eta = eta
-    self.rho = rho
+    self.optim = optimizer
 
-    self._phi_est = None
-    self.reset()
-
-  def reset(self):
-    self._phi_est = None
-
-  def step(self, X):
+  def phi(self, X):
     X = X.detach().requires_grad_(True)
 
     log_prob = self.P.log_prob(X)
-    score_func = torch.autograd.grad(log_prob, X,
-                                     grad_outputs=torch.ones_like(log_prob),
-                                     only_inputs=True)[0]
+    score_func = autograd.grad(log_prob, X,
+                               grad_outputs=torch.ones_like(log_prob),
+                               only_inputs=True)[0]
 
     K_XX = self.K(X, X.detach())
-    grad_K = -torch.autograd.grad(K_XX, X,
-                                  grad_outputs=torch.ones_like(K_XX),
-                                  only_inputs=True)[0]
+    grad_K = -autograd.grad(K_XX, X,
+                            grad_outputs=torch.ones_like(K_XX),
+                            only_inputs=True)[0]
 
     phi = (K_XX.detach().matmul(score_func) + grad_K) / X.size(0)
 
-    if self._phi_est is None:
-      self._phi_est = phi ** 2
-    else:
-      self._phi_est = self.rho * self._phi_est + (1 - self.rho) * phi ** 2
+    return phi
 
-    grad = phi / (1e-8 + self._phi_est.sqrt())
-
-    return X.detach() + self.eta * grad
+  def step(self, X):
+    self.optim.zero_grad()
+    X.grad = -self.phi(X)
+    self.optim.step()
